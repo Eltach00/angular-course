ea22ae6ff28e883eef62cc66be7e7119dfb823b4
Eltach00 <eltacquliyev@gmail.com>
2023-01-16 18:05:31 +0400

    router begin

--- helloapp/src/app/app.component.ts
+++ helloapp/src/app/app.component.ts
@@ -1,46 +1,10 @@
-import { Component, OnInit } from '@angular/core'
-import { GetService } from './node-http.service'
-import { User } from './user'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'my-app',
-  template: `<div class="form-group">
-      <label>Имя</label>
-      <input class="form-control" name="username" [(ngModel)]="user.name" />
-    </div>
-    <div class="form-group">
-      <label>Возраст</label>
-      <input
-        class="form-control"
-        type="number"
-        name="age"
-        [(ngModel)]="user.age"
-      />
-    </div>
-    <div class="form-group">
-      <button class="btn btn-default" (click)="submit(user)">Отправить</button>
-    </div>
-    <div *ngIf="done">
-      <div>Получено от сервера:</div>
-      <div>Имя: {{ recievedUser?.name }}</div>
-      <div>Возраст: {{ recievedUser?.age }}</div>
-    </div>`,
-  providers: [GetService],
+  template: `<div>
+    <h1>Маршрутизация в Angular 15</h1>
+    <router-outlet></router-outlet>
+  </div>`,
 })
-export class AppComponent {
-  done = false
-  user = new User('', 0)
-  recievedUser: User | undefined
-  constructor(private httpService: GetService) {}
-  submit(user: User) {
-    this.httpService.postUser(user).subscribe({
-      next: (data: any) => {
-        this.recievedUser = data
-        this.done = true
-      },
-      error: (err) => {
-        console.log(err.message)
-      },
-    })
-  }
-}
+export class AppComponent {}
--- helloapp/src/app/app.module.ts
+++ helloapp/src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
+import { Routes, RouterModule } from '@angular/router'
+import { AboutComponent } from './about.component'
+import { HomeComponent } from './home.component'
+import { NotFoundComponent } from './not-found.component'
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent },
+]
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
